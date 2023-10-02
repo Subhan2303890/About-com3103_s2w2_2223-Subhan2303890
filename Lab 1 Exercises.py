762719d6ea34984107cbce25f5c5350db33ebfbe
Subhan2303890 <146081159+Subhan2303890@users.noreply.github.com>
2023-10-02 13:27:36 +0100

    Add files via upload

---
+++ Lab 1 Exercises.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+def main ():
+
+    if __name__ == "__main__":
+        main ()
+
+
+# In[12]:
+
+
+print ("Hello World");
+
+
+# In[1]:
+
+
+print("Subhan") 
+
+
+# In[3]:
+
+
+a = "Hello, World!" 
+print(len(a))
+
+
+# In[13]:
+
+
+a = "Hello World!"
+print(a.upper())
+
+
+# In[14]:
+
+
+a = "Hello World!"
+print(a.lower())
+
+
+# In[ ]:
+
+
+user_height_cm = 0
+inputed = input("Enter your height: ")
+inputed = int(inputed)
+
+
+# In[ ]:
+
+
+
+
